afe11ae388aca4267497439895852110f98db8eb
Tanushree Paul <141433373+Tanushree-Paul@users.noreply.github.com>
2026-06-03 10:27:56 +0530

    Time: 2 ms (99.39%) | Memory: 47.5 MB (11.00%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,38 +1,43 @@
 class Solution {
-    class Pair {
-        int index, value;
-        Pair (int value, int index){
-            this.index = index;
-            this.value = value;
-        }
-    }
+    // class Pair{
+    //     int index; 
+    //     int value;
+    //     Pair (int value, int index){
+    //         this.value = value;
+    //         this.index = index;
+    //     }
+    // }
+    // public int[] twoSum(int[] nums, int target) {
+    //     Pair [] arr = new Pair [nums.length];
+    //     for (int i = 0; i < nums.length; i++){
+    //         arr[i] = new Pair (nums[i], i);
+    //     }
+    //     Arrays.sort(arr, (a,b) -> a.value - b.value);
+    //     int left = 0, right = nums.length - 1;
+    //     while (left < right){
+    //         int sum = arr[left].value + arr[right].value;
+    //         if (sum == target){
+    //             return new int [] {arr[left].index, arr[right].index};
+    //         }
+    //         else if(sum > target){
+    //             right --;
+    //         }
+    //         else left++;
+    //     }
+    //     return new int [] {-1, -1};
+    // 
     public int[] twoSum(int[] nums, int target) {
-        //store the value of the indices
-        int n = nums.length;
-        Pair[] arr = new Pair[n];
-        for (int i = 0; i<n; i++){
-            arr[i] = new Pair(nums[i], i);
-        }
-        //based on value 
-        Arrays.sort(arr, (a,b) -> a.value - b.value);
-
-        int left =0, right = n - 1;
-        while (left<right){
-            int sum = arr[left].value + arr[right].value;
-            if(sum == target){
-                return new int[] {arr[left].index, arr[right].index};
-            }
-            else if(sum < target) left++;
-            else right--;
-        }   
-        /*HashMap <Integer, Integer> map = new HashMap <> ();
-        for (int i = 0; i<nums.length; i++){
+        HashMap <Integer, Integer> map = new HashMap <>();
+        for (int i = 0; i < nums.length ; i++){
             int complement = target - nums[i];
             if (map.containsKey(complement)){
-                return new int[] {map.get(complement), i};
+                return new int [] {i,map.get(complement)};
             }
-            map.put(nums[i], i);
-        }*/
-        return new int[] {-1,-1};
+            else {
+                map.put(nums[i], i);
+            }
+        }
+        return new int [] {-1, -1};
     }
+
 }
