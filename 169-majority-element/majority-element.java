4006f5495c85ffad2c42dcfd7b70584b9dbf7603
Tanushree Paul <141433373+Tanushree-Paul@users.noreply.github.com>
2026-06-03 11:49:25 +0530

    Time: 2 ms (70.11%) | Memory: 55.7 MB (30.81%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,38 +1,15 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        /*HashMap <Integer, Integer> map = new HashMap <> ();
-        int n = nums.length; 
-        for (int i = 0; i < n; i++){
-            int val = nums[i];
-            if (map.containsKey(val)){
-                map.put(val, map.get(val) + 1);
-            }
-            else {
-                map.put(val, 1);
-            }
-            if(map.get(val) > n/2) return val;
-        }
-        return -1; */
+        int el = 0 , count = 0;
 
-        //moore voting's algo 
-        int el = 0;
-        int count = 0;
-        for (int num: nums){
+        for (int i =0 ; i < nums.length; i++){
             if (count == 0){
-                el = num;
-            }
-            if (num == el) count ++;
-            else count --; 
+                count = 1;
+                el = nums[i];
+            }            
+            else if (nums[i] == el) count ++;
+            else count--;
         }
-        return el;
-
-        /*
-        count = 0;
-        for(int num : nums){
-            if (num = el) count ++;
-        }
-        if (count > nums.length/2) return el;
-        return -1;
-        */
+        return el ;
     }
 }
