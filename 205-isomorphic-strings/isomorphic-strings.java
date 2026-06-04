8c9339eee1430d2cbb1e8b16b6cc35b083645e5f
Tanushree Paul <141433373+Tanushree-Paul@users.noreply.github.com>
2026-06-04 11:51:00 +0530

    Time: 17 ms (39.03%) | Memory: 44 MB (25.97%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,38 +1,25 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        /*if (s.length() != t.length()) return false;
-        Map <Character, Character> map = new HashMap <> ();
-        for (int i = 0; i<s.length(); i++){
-            char c1 = s.charAt(i);
-            char c2 = t.charAt(i);
-            if (map.containsKey(c1)){
-                if (map.get(c1) != c2) return false;
+        if(s.length() != t.length()) return false;
+        HashMap <Character, Character> mapst = new HashMap <>();
+        HashMap <Character, Character> mapts = new HashMap <>();
+        for(int i = 0; i<s.length(); i++){
+            char cs = s.charAt(i);
+            char ct = t.charAt(i);
+            if(mapst.containsKey(cs)){
+                if(mapst.get(cs) != ct) return false;
             }
             else{
-                if (map.containsValue(c2)) return false;
-                map.put(c1, c2);
-            }   
-        }
-        return true; */
-        if (s.length() != t.length()) return false;
-        Map <Character, Character> mapST = new HashMap <> ();
-        Map <Character, Character> mapTS = new HashMap <> ();
-        for (int i = 0; i<s.length(); i++){
-            char c1 = s.charAt(i);
-            char c2 = t.charAt(i);
-            if (mapST.containsKey(c1)){
-                if (mapST.get(c1)!= c2) return false;
+                mapst.put(cs, ct);
+            }
+            if(mapts.containsKey (ct)){
+                if(mapts.get(ct) != cs) return false;
             }
             else{
-                mapST.put(c1,c2);
-            }
-            if (mapTS.containsKey(c2)){
-                if (mapTS.get(c2)!= c1) return false;
-            }
-            else{
-                mapTS.put(c2,c1);
+                mapts.put(ct, cs);
             }
         }
         return true;
     }
+
 }
