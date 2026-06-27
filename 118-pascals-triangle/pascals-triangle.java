f657518cf321303253eb16bbc10e03fdb3e84197
Tanushree Paul <141433373+Tanushree-Paul@users.noreply.github.com>
2026-06-28 01:32:26 +0530

    Time: 1 ms (93.83%) | Memory: 43.5 MB (51.39%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,21 +1,32 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List <List<Integer> > ans = new ArrayList <>();
-        for (int i = 0; i < numRows; i++){
-            List <Integer> row = new ArrayList <>();
-            for (int j = 0 ; j <= i; j++){
-                row.add(ncr(i, j));
-            } 
-            ans.add(row);
+    //     List <List<Integer> > ans = new ArrayList <>();
+    //     for (int i = 0; i < numRows; i++){
+    //         List <Integer> row = new ArrayList <>();
+    //         for (int j = 0 ; j <= i; j++){
+    //             row.add(ncr(i, j));
+    //         } 
+    //         ans.add(row);
+    //     }
+    //     return ans; 
+    // }
+    // public int ncr (int n, int r){
+    //     int prod = 1;
+    //     for (int i = 0; i < r; i++){
+    //         prod *= n-i; 
+    //         prod /= i+1; 
+    //     }
+    //     return prod;
+    List <List<Integer>> ans = new ArrayList<>();
+        for(int i = 0 ; i < numRows; i++){
+            List <Integer> rows = new ArrayList<>();
+            long prod = 1;
+            for(int j = 0; j <= i; j++){
+                rows.add((int) prod);
+                prod = prod * (i - j )/ (j + 1);  
+            }
+            ans.add(rows);
         }
-        return ans; 
-    }
-    public int ncr (int n, int r){
-        int prod = 1;
-        for (int i = 0; i < r; i++){
-            prod *= n-i; 
-            prod /= i+1; 
-        }
-        return prod;
+        return ans;
     }
 }
