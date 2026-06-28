6c49926a856c82a09851f1f29c77d33f794638af
Tanushree Paul <141433373+Tanushree-Paul@users.noreply.github.com>
2026-06-28 13:28:41 +0530

    Time: 1 ms (99.96%) | Memory: 46.4 MB (56.50%) - LeetSync

--- 383-ransom-note/ransom-note.java
+++ 383-ransom-note/ransom-note.java
@@ -1,17 +1,28 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        int countRansom [] = new int[26];
-        int countMaga [] = new int[26];
-        for (char c : ransomNote.toCharArray()){
-            countRansom[c - 'a']++;
-        }
+        // int countRansom [] = new int[26];
+        // int countMaga [] = new int[26];
+        // for (char c : ransomNote.toCharArray()){
+        //     countRansom[c - 'a']++;
+        // }
+        // for (char c : magazine.toCharArray()){
+        //     countMaga[c - 'a']++;
+        // }
+        // for (int i = 0; i < 26; i++){
+        //     if(countRansom [i] > countMaga[i]){
+        //         return false;
+        //     } 
+        // }
+        // return true;
+        int count [] = new int [26];
         for (char c : magazine.toCharArray()){
-            countMaga[c - 'a']++;
+            count[c - 'a']++;
         }
-        for (int i = 0; i < 26; i++){
-            if(countRansom [i] > countMaga[i]){
+        for (char c : ransomNote.toCharArray()){
+            count[c - 'a']--;
+            if (count[c - 'a'] < 0) {
                 return false;
-            } 
+            }
         }
         return true;
     }
