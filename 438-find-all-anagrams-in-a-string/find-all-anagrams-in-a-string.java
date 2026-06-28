c352fa0d2cf11ed74e523acb15ed57798c240838
Tanushree Paul <141433373+Tanushree-Paul@users.noreply.github.com>
2026-06-28 12:48:43 +0530

    Time: 10 ms (81.49%) | Memory: 46.9 MB (58.54%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,17 +1,48 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List <Integer> list = new ArrayList <> ();
-        char arrP [] = p.toCharArray();
-        Arrays.sort(arrP);
-        int length = arrP.length;
-        for (int i = 0 ; i <= s.length() - length; i++){
-            String sub = s.substring(i, i + length);
-            char arrSub [] = sub.toCharArray();
-            Arrays.sort(arrSub);
-            if(Arrays.equals(arrSub, arrP)){
-                list.add(i);            
+        // List <Integer> list = new ArrayList <> ();
+        // char arrP [] = p.toCharArray();
+        // Arrays.sort(arrP);
+        // int length = arrP.length;
+        // for (int i = 0 ; i <= s.length() - length; i++){
+        //     String sub = s.substring(i, i + length);
+        //     char arrSub [] = sub.toCharArray();
+        //     Arrays.sort(arrSub);
+        //     if(Arrays.equals(arrSub, arrP)){
+        //         list.add(i);            
+        //     }
+        // }
+        // return list;
+        List<Integer> ans = new ArrayList<>();
+        if (p.length() > s.length()) {
+            return ans;
+        }
+        int countP [] = new int [26];
+        for(char c : p.toCharArray()){
+            countP[c - 'a']++;
+        }
+        int[] window = new int[26];
+        int length = p.length();
+
+        //first window
+        for(int i = 0; i < length; i++){
+            window[s.charAt(i) - 'a']++;
+        }
+        if (Arrays.equals(countP, window)) {
+            ans.add(0);
+        }
+
+        // Slide the window
+        for (int i = length; i < s.length(); i++) {
+            // Add new character
+            window[s.charAt(i) - 'a']++;
+
+            // Remove old character
+            window[s.charAt(i - length) - 'a']--;
+            if (Arrays.equals(countP, window)) {
+                ans.add(i - length + 1);
             }
         }
-        return list;
+        return ans;
     }
 }
