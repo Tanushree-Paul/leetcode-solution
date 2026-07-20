90c01cea1a4b6ac8e0828f57cd1e8a1cc05d474c
Tanushree Paul <141433373+Tanushree-Paul@users.noreply.github.com>
2026-07-20 17:40:08 +0530

    Time: 5 ms (83.45%) | Memory: 46.9 MB (92.18%) - LeetSync

--- 1386-shift-2d-grid/shift-2d-grid.java
+++ 1386-shift-2d-grid/shift-2d-grid.java
@@ -1,34 +1,64 @@
 class Solution {
     public List<List<Integer>> shiftGrid(int[][] grid, int k) {
+    //     int n = grid.length;
+    //     int m = grid[0].length;
+    //     int len = n * m;
+
+    //     k = k % len;
+
+    //     rev (grid, 0, len- k - 1, m);
+    //     rev (grid, len - k, len -1, m);
+    //     rev (grid, 0, len -1, m);
+
+    //     List<List<Integer>> ans = new ArrayList<>();
+    //     for (int i = 0; i < n; i++) {
+    //         List<Integer> row = new ArrayList<>();
+
+    //         for (int j = 0; j < m; j++) {
+    //             row.add(grid[i][j]);
+    //         }
+
+    //         ans.add(row);
+    //     }
+    //     return ans;
+    // }
+    // public void rev(int [][]num, int i , int j, int col){
+    //     while(i < j){
+    //         int temp = num[i/col][i%col];
+    //         num[i/col][i%col] = num[j/col][j%col];
+    //         num[j/col][j%col] = temp;
+    //         i++;
+    //         j--;
+    //     }
+        
         int n = grid.length;
         int m = grid[0].length;
-        int len = n * m;
+        int total = n * m;
 
-        k = k % len;
+        k = k % total;
 
-        rev (grid, 0, len- k - 1, m);
-        rev (grid, len - k, len -1, m);
-        rev (grid, 0, len -1, m);
+        int[][] res = new int[n][m];
+        for(int i = 0; i < n; i++){
 
-        List<List<Integer>> ans = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            List<Integer> row = new ArrayList<>();
+            for(int j = 0; j < m; j++){
+                int idx = i * m + j;
+                int newIdx = (idx + k) % total;
 
-            for (int j = 0; j < m; j++) {
-                row.add(grid[i][j]);
+                int row = newIdx / m;
+                int col = newIdx % m;
+
+                res[row][col] = grid[i][j];
             }
+        }
 
-            ans.add(row);
+        List<List<Integer>> ans = new ArrayList <>();
+        for(int i = 0; i < n; i++){
+            List<Integer> list = new ArrayList<>();
+            for(int j = 0; j < m; j++){
+                list.add(res[i][j]);
+            }
+            ans.add(list);
         }
         return ans;
-    }
-    public void rev(int [][]num, int i , int j, int col){
-        while(i < j){
-            int temp = num[i/col][i%col];
-            num[i/col][i%col] = num[j/col][j%col];
-            num[j/col][j%col] = temp;
-            i++;
-            j--;
-        }
     }
 }
