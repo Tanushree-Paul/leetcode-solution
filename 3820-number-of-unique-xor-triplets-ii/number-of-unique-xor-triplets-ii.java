a10c8c6aa8569d3497d02787aa33eb9a9cbbc076
Tanushree Paul <141433373+Tanushree-Paul@users.noreply.github.com>
2026-07-24 23:42:55 +0530

    Time: 659 ms (32.73%) | Memory: 47.2 MB (30.91%) - LeetSync

---
+++ 3820-number-of-unique-xor-triplets-ii/number-of-unique-xor-triplets-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+        final int MAX = 2048;
+
+        boolean[] present = new boolean[MAX];
+        for (int x : nums)
+            present[x] = true;
+
+        boolean[] pair = new boolean[MAX];
+        for (int a = 0; a < MAX; a++) {
+            if (!present[a]) continue;
+            for (int b = 0; b < MAX; b++) {
+                if (!present[b]) continue;
+                pair[a ^ b] = true;
+            }
+        }
+
+        boolean[] ans = new boolean[MAX];
+        for (int x = 0; x < MAX; x++) {
+            if (!pair[x]) continue;
+            for (int a = 0; a < MAX; a++) {
+                if (!present[a]) continue;
+                ans[x ^ a] = true;
+            }
+        }
+
+        int cnt = 0;
+        for (boolean b : ans)
+            if (b) cnt++;
+
+        return cnt;
+    }
+}
