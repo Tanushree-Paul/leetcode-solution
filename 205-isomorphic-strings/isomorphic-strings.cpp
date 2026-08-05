420783eb94584503db6a159daf71b4d97ea4dd16
Tanushree Paul <141433373+Tanushree-Paul@users.noreply.github.com>
2026-08-05 20:45:10 +0530

    Time: 3 ms (40.89%) | Memory: 9.4 MB (30.55%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -7,11 +7,17 @@ public:
         for(int i = 0; i <s.size(); i++){
             char cs = s[i];
             char ct = t[i];
-            if(st.find(cs) == st.end()) st[cs] = ct;
-            else if(st[cs] != ct) return false;
+            // if(st.find(cs) == st.end()) st[cs] = ct;
+            // else if(st[cs] != ct) return false;
 
-            if(ts.find(ct) == ts.end()) ts[ct] = cs;
-            else if(ts[ct] != cs) return false;
+            // if(ts.find(ct) == ts.end()) ts[ct] = cs;
+            // else if(ts[ct] != cs) return false;
+
+            if(st.count(cs) && st[cs] != ct) return false;
+            if(ts.count(ct) && ts[ct] != cs) return false;
+
+            st[cs] = ct;
+            ts[ct] = cs;
         }
         return true;
     }
