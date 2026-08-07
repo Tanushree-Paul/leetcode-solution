ce7e9c8a4cf8cc8f2f827763ff74222a0ecc5616
Tanushree Paul <141433373+Tanushree-Paul@users.noreply.github.com>
2026-08-07 14:03:39 +0530

    Time: 2 ms (94.69%) | Memory: 47.8 MB (35.48%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,21 +1,39 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
+        // int n = nums.length;
+        // int [] arr = new int[2];
+        // HashSet <Integer> set = new HashSet<>();
+        // for (int i : nums){
+        //     if(set.contains(i)){
+        //         arr[0] = i;
+        //     }
+        //     set.add(i);
+        // }
+        // for (int i = 1; i <= nums.length; i++){
+        //     if(!set.contains(i)){
+        //         arr[1] = i;
+        //         break;
+        //     }
+        // }
+        // return arr;
         int n = nums.length;
-        int [] arr = new int[2];
-        HashSet <Integer> set = new HashSet<>();
-        for (int i : nums){
-            if(set.contains(i)){
-                arr[0] = i;
-            }
-            set.add(i);
+        int actualSum = 0;
+        long actualSqSum = 0;
+        for(int i : nums){
+            actualSum += i;
+            actualSqSum += (long) i*i;
         }
-        for (int i = 1; i <= nums.length; i++){
-            if(!set.contains(i)){
-                arr[1] = i;
-                break;
-            }
-        }
-        return arr;
+
+        int expectedSum = n * (n+1) /2;
+        long expectedSqSum = (long) n * (n + 1) * (2*n + 1) / 6;
+
+        int diff = actualSum - expectedSum;
+        long sum = (actualSqSum - expectedSqSum) / diff;
+
+        int x = (int) (diff + sum) / 2 ;
+        int y = x - diff;
+
+        return new int[] {x, y};
     }
 
 }
