6b35ee713e5be18af61924b06c2bf77ff2292230
Tanushree Paul <141433373+Tanushree-Paul@users.noreply.github.com>
2026-08-13 13:59:56 +0530

    Time: 6 ms (77.02%) | Memory: 47.9 MB (15.80%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -17,32 +17,51 @@ class Solution {
     // }
 
 
-    // HashMap <Character, Integer> map = new HashMap <> ();
-    // int max = 0;
-    // int left = 0;
-    
-    // for (int right = 0; right < s.length(); right++){
-    //     if (map.containsKey(s.charAt(right))){
-    //         left = Math.max(left, map.get(s.charAt(right)) + 1);
-    //     }
-    //     map.put(s.charAt(right), right);
-    //     int length = right - left + 1;
-    //     max = Math.max(max, length);
-    // }
-    // return max;
+        // HashMap <Character, Integer> map = new HashMap <> ();
+        // int max = 0;
+        // int left = 0;
+        
+        // for (int right = 0; right < s.length(); right++){
+        //     if (map.containsKey(s.charAt(right))){
+        //         left = Math.max(left, map.get(s.charAt(right)) + 1);
+        //     }
+        //     map.put(s.charAt(right), right);
+        //     int length = right - left + 1;
+        //     max = Math.max(max, length);
+        // }
+        // return max;
+
+
+        // int left = 0;
+        // int right = 0;
+        // int max = 0;
+        // int freq [] = new int[256];
+        // for(right = 0; right < s.length(); right++){
+        //     while(freq[s.charAt(right)] == 1){
+        //         freq[s.charAt(left)]--; 
+        //         left++;
+        //     }
+        //     freq[s.charAt(right)]++;
+
+        //     max = Math.max(max, right - left + 1);
+        // }
+        // return max;
+
 
         int left = 0;
         int right = 0;
+        int hash [] = new int[256];
         int max = 0;
-        int freq [] = new int[256];
-        for(right = 0; right < s.length(); right++){
-            while(freq[s.charAt(right)] == 1){
-                freq[s.charAt(left)]--; 
-                left++;
+        while(right < s.length()){
+            char ch = s.charAt(right);
+            if(hash[ch] > 0){
+                if(hash[ch] > left){
+                    left = hash[ch];
+                }
             }
-            freq[s.charAt(right)]++;
-
+            hash[ch] = right + 1; 
             max = Math.max(max, right - left + 1);
+            right++;
         }
         return max;
     }
